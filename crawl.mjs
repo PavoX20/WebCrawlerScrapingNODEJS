7702598f12a1d41f2a3c3125f08b42671818dcda
Pablo Sarzosa <pavosarzosa20@gmail.com>
2024-02-26 19:50:41 -0500

    crawl single page with error manage

--- crawl.mjs
+++ crawl.mjs
@@ -1,31 +1,57 @@
-import {JSDOM} from 'jsdom';
+import { JSDOM } from "jsdom";
+
+export function crawlPage(currentURL) {
+  console.log(`actively ${currentURL}`);
+  
+    fetch(currentURL)
+    .then((response) => {
+
+      if (!response.ok) {
+        console.log('Error in fetching on: '+currentURL+' with status: '+response.status+' '+response.statusText);
+        return (process.exit(1));
+      }
+
+      const contentType = response.headers.get("content-type");
+      if(!contentType.includes('text/html')){
+        console.log('Non HTML response, content type: '+contentType+' with status: '+response.status+' '+response.statusText);
+        return (process.exit(1));
+      }
+
+      return response.text();
+    })
+    .then((htmlBody) => {
+      console.log(htmlBody);
+    })
+    .catch((error) => console.log('Error in fetching on: '+currentURL));
+  
+}
 
 export function getURLsFromPage(htmlBody, baseURL) {
-    
-    const urls= []
-    const dom = new JSDOM(htmlBody)
-    const linkElements = dom.window.document.querySelectorAll('a')
-    
-    for (const linkElement of linkElements) {
-        if(linkElement.href.slice(0,1)==='/') {
-            //Relative
-            urls.push(`${baseURL}${linkElement.href}`)
-        }else if(linkElement.href.slice(0,4)==='http'){
-            //Absolute
-            urls.push(linkElement.href)
-        }else{
-            //ignore
-        }
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
 
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //Relative
+      urls.push(`${baseURL}${linkElement.href}`);
+    } else if (linkElement.href.slice(0, 4) === "http") {
+      //Absolute
+      urls.push(linkElement.href);
+    } else {
+      //ignore
     }
-    
-    return urls
+  }
+
+  return urls;
 }
 
 export function normalizeURL(urlString) {
-    const urlObject = new URL(urlString);
-    const domain = urlObject.hostname;
-    const path = urlObject.pathname.endsWith('/') ? urlObject.pathname.slice(0, -1) : urlObject.pathname;
-    
-    return `${domain}${path}`;
+  const urlObject = new URL(urlString);
+  const domain = urlObject.hostname;
+  const path = urlObject.pathname.endsWith("/")
+    ? urlObject.pathname.slice(0, -1)
+    : urlObject.pathname;
+
+  return `${domain}${path}`;
 }
--- main.mjs
+++ main.mjs
@@ -1,1 +1,19 @@
-console.log("Hello from main.mjs")
+import { crawlPage } from "./crawl.mjs"
+
+function main(){
+    if(process.argv.length < 3){
+        console.log("No website provided")
+        process.exit(1)
+    }
+    if(process.argv.length > 3){
+        console.log("Too many arguments")
+        process.exit(1)
+    }
+    const website = process.argv[2]
+
+    console.log(`Crawling ${website}`)
+
+    crawlPage(website)
+}
+
+main()
